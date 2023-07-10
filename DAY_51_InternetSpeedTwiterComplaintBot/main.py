22c63c70ae66a12aa5763be745cf4cd038dcd8b4
Kushalregmi <kushalbro82@gmail.com>
2023-07-10 10:00:00 +0545

    Uploading Day_51 InternaetSpeed Twiter Complaint Bot

--- DAY_51_InternetSpeedTwiterComplaintBot/main.py
+++ DAY_51_InternetSpeedTwiterComplaintBot/main.py
@@ -24,26 +24,63 @@ class InternetSpeedTwitterBot:
         # Configure Chrome options
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_experimental_option("detach", True)
+        
+        # Add options to disable GPU hardware acceleration
+        self.chrome_options.add_argument("--disable-gpu")
+        self.chrome_options.add_argument("--disable-software-rasterizer")
 
         # Start WebDriver
         self.service = Service(webdriver_path)
         self.driver = webdriver.Chrome(service=self.service, options=self.chrome_options)
         
-        self.down=0
-        self.up = 0
+        self.down = 0
+        self.upload = 0
         
-        
+    # Function to get the internet speed using speedtest website    
     def get_internet_speed(self):
         pass
-    
+    #     self.driver.get('https://www.speedtest.net')
+    #     go_button = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a'
+    #     self.wait_and_click(self.driver, go_button)
+        
+    # # Getting hold of upload and download Internet speed
+
+    #     self.upload=WebDriverWait(self.driver, 20).until(
+    #             EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span'))
+    #             ).text
+    #     time.sleep(2)
+    #     self.down = self.driver.find_element(By.XPATH,
+    #                                          value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span'
+    #                                          ).text
+        # self.driver.quit()
+        
+    #     print("upload_speed= ",self.upload)
+    #     print("Download_speed= ",self.down)
+        
+    # Method to make a complaint tweet
     def tweet_at_provider(self):
-        pass
+        # time.sleep(4)
+        self.driver.get('https://x.com')
+        time.sleep(2)
+        signIN_xpath='//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[2]'
+        # signIn_button= WebDriverWait(self.driver, 20).until(
+        #         EC.presence_of_element_located((By.XPATH,signIN_xpath))
+        #         )
+        # signIn_button.click()
+        self.wait_and_click(self.driver, signIN_xpath)
+        
+    # Method to click on buttons
+    def wait_and_click(self, driver, xpath, timeout=20):
+        try:
+            element = WebDriverWait(driver, timeout).until(
+                EC.element_to_be_clickable((By.XPATH, xpath))
+            )
+            element.click()
+        except (TimeoutException, NoSuchElementException, ElementClickInterceptedException) as e:
+            print(f"Exception during wait_and_click: {e}")
     
     
-#TODO: CREATING OBJECT
+# Creating object
 bot = InternetSpeedTwitterBot()
 bot.get_internet_speed()   
-bot.tweet_at_provider()     
-        
-        
-        
+bot.tweet_at_provider()
