281c4c1f71b34031d00b6d5fe587aa3072989b02
Kushalregmi <kushalbro82@gmail.com>
2024-07-16 18:12:54 +0545

    Making final upgrade to day_51 project.

--- DAY_51_InternetSpeedTwiterComplaintBot/main.py
+++ DAY_51_InternetSpeedTwiterComplaintBot/main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException, ElementClickInterceptedException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException
 import time
 import os
 from dotenv import load_dotenv
@@ -13,8 +13,8 @@ from dotenv import load_dotenv
 load_dotenv()
 EMAIL = os.getenv('GF_FB_EMAIL')
 PASSWORD = os.getenv('TWITTER_PASS')
-PROMISE_UPLOAD=20
-PROMISE_DOWN=100
+PROMISE_UPLOAD = 80
+PROMISE_DOWN = 100
 
 # Path to your ChromeDriver executable
 webdriver_path = 'C:\\Program Files\\Google\\Chrome_Driver\\chromedriver.exe'
@@ -24,8 +24,6 @@ class InternetSpeedTwitterBot:
         # Configure Chrome options
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_experimental_option("detach", True)
-        
-        # Add options to disable GPU hardware acceleration
         self.chrome_options.add_argument("--disable-gpu")
         self.chrome_options.add_argument("--disable-software-rasterizer")
 
@@ -36,51 +34,97 @@ class InternetSpeedTwitterBot:
         self.down = 0
         self.upload = 0
         
-    # Function to get the internet speed using speedtest website    
     def get_internet_speed(self):
-        pass
-    #     self.driver.get('https://www.speedtest.net')
-    #     go_button = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a'
-    #     self.wait_and_click(self.driver, go_button)
-        
-    # # Getting hold of upload and download Internet speed
-
-    #     self.upload=WebDriverWait(self.driver, 20).until(
-    #             EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span'))
-    #             ).text
-    #     time.sleep(2)
-    #     self.down = self.driver.find_element(By.XPATH,
-    #                                          value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span'
-    #                                          ).text
-        # self.driver.quit()
-        
-    #     print("upload_speed= ",self.upload)
-    #     print("Download_speed= ",self.down)
-        
-    # Method to make a complaint tweet
-    def tweet_at_provider(self):
-        # time.sleep(4)
-        self.driver.get('https://x.com')
+        self.driver.get('https://www.speedtest.net')
+        go_button_xpath = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a'
+        self.wait_and_click(go_button_xpath)
+        time.sleep(60)
+        self.upload = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span'))
+        ).text
         time.sleep(2)
-        signIN_xpath='//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[2]'
-        # signIn_button= WebDriverWait(self.driver, 20).until(
-        #         EC.presence_of_element_located((By.XPATH,signIN_xpath))
-        #         )
-        # signIn_button.click()
-        self.wait_and_click(self.driver, signIN_xpath)
+        self.down = self.driver.find_element(By.XPATH,
+                                             '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span'
+                                             ).text
+        time.sleep(4)
+        print("Upload Speed:", self.upload)
+        print("Download Speed:", self.down)
         
-    # Method to click on buttons
-    def wait_and_click(self, driver, xpath, timeout=20):
+        
+    def tweet_at_provider(self):
+        self.driver.get('https://twitter.com/login')
+        time.sleep(10)
+
+        # Logging in to Twitter
+        email_xpath = '//input[@name="text"]'
+        self.wait_and_send_keys(email_xpath, EMAIL)
+
+        password_xpath = '//input[@name="password"]'
+        self.wait_and_send_keys(password_xpath, PASSWORD)
+
+        # Handle CAPTCHA if it appears
+        self.handle_captcha()
+
+        # Tweeting
+        time.sleep(8)
+        tweet_box_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div'
+        tweet_text = f"My internet speed is {self.down} Mbps download / {self.upload} Mbps upload, but I pay for {PROMISE_DOWN} Mbps / {PROMISE_UPLOAD} Mbps. #InternetSpeed"
+        
+        tweet_box = WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, tweet_box_xpath))
+        )
+        tweet_box.send_keys(tweet_text)
+
+        time.sleep(7)
+        tweet_button_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/button' 
         try:
-            element = WebDriverWait(driver, timeout).until(
+            tweet_button = WebDriverWait(self.driver, 20).until(
+                    EC.presence_of_element_located((By.XPATH, tweet_button_xpath))
+                )
+            self.driver.execute_script('arguments[0].click()', tweet_button)
+        except (TimeoutException, NoSuchElementException, ElementClickInterceptedException) as e:
+            print(f"Exception during wait_and_send_keys: {e}")
+            
+            
+            
+    def wait_and_click(self, xpath, timeout=20):
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
                 EC.element_to_be_clickable((By.XPATH, xpath))
             )
             element.click()
         except (TimeoutException, NoSuchElementException, ElementClickInterceptedException) as e:
             print(f"Exception during wait_and_click: {e}")
-    
-    
+            
+    def wait_and_send_keys(self, xpath, keys, timeout=20):
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located((By.XPATH, xpath))
+            )
+            element.send_keys(keys, Keys.ENTER)
+        except (TimeoutException, NoSuchElementException, ElementClickInterceptedException) as e:
+            print(f"Exception during wait_and_send_keys: {e}")
+
+    def handle_captcha(self):
+        # Handle CAPTCHA if it appears
+        try:
+            captcha_element = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, '//div[@class="captcha"]'))
+            )
+            if captcha_element:
+                print("CAPTCHA detected. Please solve it manually.")
+                time.sleep(60)  # Give user time to solve CAPTCHA
+        except TimeoutException:
+            # CAPTCHA not found
+            pass
+        
+
+            
+
 # Creating object
 bot = InternetSpeedTwitterBot()
-bot.get_internet_speed()   
-bot.tweet_at_provider()
+bot.get_internet_speed()
+if float(bot.upload)<float(PROMISE_UPLOAD) and float(bot.down)<float(PROMISE_DOWN):
+    bot.tweet_at_provider()
+else:
+    print(f"Internet Speed is as promised.{bot.upload} up Mbps/ {bot.down} down Mbps")    
