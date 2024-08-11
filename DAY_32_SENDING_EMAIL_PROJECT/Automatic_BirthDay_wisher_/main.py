40c3f6438dd89b79dcb1d6a2b326f132c624a79a
Kushalregmi61 <kushalbro82@gmail.com>
2024-08-11 19:51:35 +0545

    fix the file path error in Day_32

--- DAY_32_SENDING_EMAIL_PROJECT/Automatic_BirthDay_wisher_/main.py
+++ DAY_32_SENDING_EMAIL_PROJECT/Automatic_BirthDay_wisher_/main.py
@@ -1,43 +1,39 @@
-##################### Birthday  E-mail Project ######################
 import smtplib
 import random
 import datetime as dt
-import pandas
+import pandas as pd
 import os
 from dotenv import load_dotenv
 
-#loading environment variable 
+# Load environment variables
 load_dotenv()
 
-my_email = os.getenv("my_email") #my email id
-my_password= os.getenv("EMAIL_PASSWORD") #random id password
+my_email = os.getenv("my_email")  # My email ID
+my_password = os.getenv("EMAIL_PASSWORD")  # Email password
 
-#creating a datetime class object
+# Create a datetime object
 time = dt.datetime.now()
-today_tuple = (time.day,time.month)
+today_tuple = (time.day, time.month)
 
-#TODO 1 OPENING BIRTHDAY CSV FILE TO READ BIRTHDAY FORM IT
-df = pandas.read_csv(r"DAY_32_SENDING_EMAIL_PROJECT\Automatic_BirthDay_wisher_\birthdays.csv")
+# TODO 1: Open the birthday CSV file and read birthdays from it
+df = pd.read_csv("DAY_32_SENDING_EMAIL_PROJECT/Automatic_BirthDay_wisher_/birthdays.csv")
 
-#creating birthdays_dict
-birthdays_dict = {(row.day, row.month):row for (index, row) in df.iterrows()}
+# Create a dictionary of birthdays
+birthdays_dict = {(row.day, row.month): row for (index, row) in df.iterrows()}
 
-    
-#TODO_2 checking if the date/month matches
-if today_tuple in birthdays_dict: 
-    brithday_person = birthdays_dict[today_tuple]
-    with open(f"DAY_32_SENDING_EMAIL_PROJECT\Automatic_BirthDay_wisher_\letter_templates\letter_{random.randint(1,3)}.txt")  as letter_file:
-        text_msg = letter_file.read() 
-        text_msg =text_msg.replace("[NAME]", brithday_person['name'])
+# TODO 2: Check if today's date matches any birthdays
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    with open(f"DAY_32_SENDING_EMAIL_PROJECT/Automatic_BirthDay_wisher_/letter_templates/letter_{random.randint(1, 3)}.txt") as letter_file:
+        text_msg = letter_file.read()
+        text_msg = text_msg.replace("[NAME]", birthday_person['name'])
 
-#TODO _ 3 SENDING EMAIL TO PERSON        
+    # TODO 3: Send the email
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(user  = my_email, password=my_password)
-        connection.sendmail(from_addr=my_email, to_addrs=brithday_person['email'],                    
-        msg=f"Subject: Happy Birthday!.... :) \n\n{text_msg}")
-  
-
-
-
-
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday_person['email'],
+            msg=f"Subject: Happy Birthday!.... :) \n\n{text_msg}"
+        )
