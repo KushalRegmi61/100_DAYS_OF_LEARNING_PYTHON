d5f7540604a05957eaf332d626ebcef971819261
Kushalregmi61 <kushalbro82@gmail.com>
2024-08-14 05:37:06 +0545

    Added Update Movie_Rating functionality to update button

---
+++ DAY_64_Top-10_MoviesWebsite/main.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FloatField
+from wtforms.validators import DataRequired, NumberRange
+import requests
+from flask_wtf import FlaskForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+    # rating = FloatField('Rating', validators=[
+    #     DataRequired(message="Rating cannot be empty."),
+    #     NumberRange(min=0, max=10, message="Rating must be between 0 and 10.")
+    # ])
+#Edit Movie Rating Form:
+class EditRating(FlaskForm):
+    rating=FloatField('Your rating out of 10, Eg: 7.5', 
+                      validators=[
+                                    DataRequired(message="Field cannot be empty."),
+                                    NumberRange(min=0, max=10, message="Rating must be between 0 and 10.")                                
+                                ])
+    review= StringField("Your review.", validators=[DataRequired(message="Field cannot be empty.")])
+    submit= SubmitField('Update', render_kw={"size":30})
+
+##CREATE DB
+class Base(DeclarativeBase):
+    pass
+#intializing the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+##CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+# # After adding the new_movie the code needs to be commented out/deleted.
+# # So you are not trying to add the same movie twice.
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# second_movie = Movie(
+#     title="Avatar The Way of Water",
+#     year=2022,
+#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     rating=7.3,
+#     ranking=9,
+#     review="I liked the water.",
+#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+# )
+# with app.app_context():
+#     db.session.add(new_movie)
+#     db.session.add(second_movie)
+#     db.session.commit()
+
+
+@app.route("/")
+def home():
+    result = db.session.execute(db.select(Movie))
+    all_movies = result.scalars().all()
+    return render_template("index.html", movies=all_movies)
+
+@app.route("/edit/<int:id>", methods=['POST', 'GET'])
+def edit(id):
+    form = EditRating()
+    item_to_update= Movie.query.get_or_404(id)
+    
+    if form.validate_on_submit():
+        item_to_update.rating=form.rating.data
+        item_to_update.review=form.review.data
+        # with app.app_context():
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template('edit.html', form = form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
