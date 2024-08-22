e9ea69669c51d28732a9053bb0fe9a011b9d827f
Kushalregmi61 <kushalbro82@gmail.com>
2024-08-22 21:46:22 +0545

    added admin_only decorator that only allows admin of the website to add , edit and delete blog post

--- DAY_69_BlogWebsite-Final-Part/forms.py
+++ DAY_69_BlogWebsite-Final-Part/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, ValidationError, Email, Length
 from flask_ckeditor import CKEditorField
+import re
 
 
 # WTForm for creating a blog post
@@ -13,10 +14,30 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
+# Custom validator function
+def validate_special_characters(form, field):
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', field.data):
+        raise ValidationError("Password must contain at least one special character.")
+    
 # TODO: Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    name= StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', render_kw={"size": 30}, validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=8, message="Too short. Must have 8 characters."),
+        validate_special_characters
+    ])
+    submit = SubmitField(label="SignMe up!", render_kw={"size": 30})
+
+
 
 
 # TODO: Create a LoginForm to login existing users
+class LoginForm(FlaskForm):
+    email = StringField('Email', render_kw={"size": 30}, validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField(label="Log In", render_kw={"size": 30})
 
 
 # TODO: Create a CommentForm so users can leave comments below posts
--- DAY_69_BlogWebsite-Final-Part/main.py
+++ DAY_69_BlogWebsite-Final-Part/main.py
@@ -1,37 +1,48 @@
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-from flask_gravatar import Gravatar
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+# from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
-from forms import CreatePostForm
+from forms import CreatePostForm, RegisterForm, LoginForm
+from functools import wraps
 
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
+#TODO: creating @adminonly decorator
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Check if the user is authenticated
+        if not current_user.is_authenticated or \
+           current_user.email != 'admin@gmail.com' or \
+           current_user.password != 'Admin@1432':
+            return abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
 
-On Windows type:
-python -m pip install -r requirements.txt
 
-On MacOS type:
-pip3 install -r requirements.txt
 
-This will install the packages from the requirements.txt for this project.
-'''
-
+#TODO: INITAILIZING FLASK APP
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 # TODO: Configure Flask-Login
+
+login_manager= LoginManager()
+login_manager.__init__(app)
+
+# Create a user_loader callback
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get_or_404(user_id)
 
 
 # CREATE DATABASE
@@ -44,7 +55,7 @@ db.init_app(app)
 
 # CONFIGURE TABLES
 class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
+    __tablename__ = "blog_post"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
@@ -55,38 +66,86 @@ class BlogPost(db.Model):
 
 
 # TODO: Create a User table for all your registered users. 
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100))
+   
 
 
 with app.app_context():
     db.create_all()
+    # blog_db.create_all()
 
 
 # TODO: Use Werkzeug to hash the user's password when creating a new user.
-@app.route('/register')
+@app.route('/register' ,methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
-
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = request.form.get('email')
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if user:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+        #for new users:
+        hash_and_salted_password = generate_password_hash(
+            form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(
+            email=form.email.data,
+            name=form.name.data,
+            password=hash_and_salted_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for("get_all_posts"))
+    
+    return render_template("register.html", form=form, logged_in=False)
 
 # TODO: Retrieve a user from the database based on their email. 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            flash("That Email doesn't exist. Try Again!")
+            return redirect(url_for('login'))
+        elif check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("get_all_posts"))
+        else:
+            flash('Password incorrect, Try again!')
+
+    return render_template("login.html", form=form, logged_in=False)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    return redirect(url_for('get_all_posts'))
+    logout_user()
+    return redirect(url_for('get_all_posts',logged_in=False))
 
 
 @app.route('/')
 def get_all_posts():
-    result = db.session.execute(db.select(BlogPost))
-    posts = result.scalars().all()
+    posts = BlogPost.query.all()
     return render_template("index.html", all_posts=posts)
 
 
 # TODO: Allow logged-in users to comment on posts
 @app.route("/post/<int:post_id>")
+@login_required
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
@@ -94,6 +153,7 @@ def show_post(post_id):
 
 # TODO: Use a decorator so only an admin user can create a new post
 @app.route("/new-post", methods=["GET", "POST"])
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -113,6 +173,7 @@ def add_new_post():
 
 # TODO: Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@admin_only
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
@@ -135,6 +196,7 @@ def edit_post(post_id):
 
 # TODO: Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
+@admin_only
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
@@ -151,6 +213,10 @@ def about():
 def contact():
     return render_template("contact.html")
 
+#TODO: METHOD TO DISPLAY OLDER POSTS
+@app.route('/older_post')
+def older_post():
+    return render_template("olderpost.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
