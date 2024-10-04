ef27663c7c45576a0e3c66f886a97702eb55f6f9
KushalRegmi61 <kushalbro82@gmail.com>
2024-10-04 11:21:31 +0545

    updated .gitignore file

--- DAY_84_TicTacToe_Game/tempCodeRunnerFile.py
+++ DAY_84_TicTacToe_Game/tempCodeRunnerFile.py
@@ -1,7 +1,3 @@
-def restart_game():
-    for widget in window.winfo_children():
-        if isinstance(widget, Button) and widget.cget("text") == " ":
-            widget.config(text=" ")
-
-restart_button = Button(text="Restart", font=("Times New Roman", 16, "bold"), command=restart_game)
-restart_button.grid(row=4, column=1, padx=10, pady=10)
+# Example usage
+draw_symbol(0, 0, "X")
+draw_symbol(1, 1, "O")
