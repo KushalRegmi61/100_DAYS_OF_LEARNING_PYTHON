f585cd9bd67c9309bdd6049394956a0a33958bb6
KushalRegmi61 <kushalbro82@gmail.com>
2024-11-05 20:57:20 +0545

    add feature to create and move pedal left and right

--- DAY_86_BreakoutGame/pedal.py
+++ DAY_86_BreakoutGame/pedal.py
@@ -1,6 +1,19 @@
 from turtle import Turtle
 
-
 class Pedal(Turtle):
-    def __init__(self, shape: str = "classic", undobuffersize: int = 1000, visible: bool = True) -> None:
-        super().__init__(shape, undobuffersize, visible)
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("White")
+        self.shapesize(stretch_len=7, stretch_wid= 1)
+        self.penup()
+        self.goto(position)
+    
+        # function to move the turtle left  and reight
+    def move_left(self):
+        new_x = self.xcor()-20
+        self.goto(new_x, self.ycor())
+
+    def move_right(self):
+        new_x = self.xcor()+20
+        self.goto(new_x, self.ycor())
