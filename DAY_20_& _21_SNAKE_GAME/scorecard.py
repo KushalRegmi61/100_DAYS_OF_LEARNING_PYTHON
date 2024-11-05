182aa414b3e82500652ae6c2c9beb9c5f8d6a0ba
KushalRegmi61 <kushalbro82@gmail.com>
2024-11-05 15:42:57 +0545

    update the scorecard label

--- DAY_20_& _21_SNAKE_GAME/scorecard.py
+++ DAY_20_& _21_SNAKE_GAME/scorecard.py
@@ -12,9 +12,9 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         #opening the file data.txt
-        with open("E:\\python\\DAY_20_& _21_SNAKE_GAME\\DAY21&22_snake_game_data.txt") as file:
+        with open("DAY_20_& _21_SNAKE_GAME/DAY21&22_snake_game_data.txt") as file:
             self.high_score = int(file.read())
-        self.goto(0, 270)
+        self.goto(0, 300)
         self.hideturtle()
         self.update_scoreboard()
 
@@ -26,7 +26,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score>=int(self.high_score):
             self.high_score = self.score
-            with open("E:\\python\\DAY_20_& _21_SNAKE_GAME\\DAY21&22_snake_game_data.txt", mode= "w") as file:
+            with open("DAY_20_& _21_SNAKE_GAME/DAY21&22_snake_game_data.txt", mode= "w") as file:
                 file.write(str(self.high_score))
             
         self.score =0
