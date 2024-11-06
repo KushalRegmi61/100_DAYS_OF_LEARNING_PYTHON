867aa130a5056eba457cd8f0488ba503f6eaddad
KushalRegmi61 <kushalbro82@gmail.com>
2024-11-06 21:11:37 +0545

    implement logic to detect if all the bricks are destroyed

--- DAY_87_BreakoutGame/main.py
+++ DAY_87_BreakoutGame/main.py
@@ -6,84 +6,65 @@ from ball import Ball
 from brick import Brick
 import time
 
-
-#global varible
+# Global variable
 is_game_on = False
 
-# Create a screen
-screen = Screen()
-
 # Set up the screen
+screen = Screen()
 screen.setup(width=800, height=800)
 screen.bgcolor("black")
 screen.title("Breakout Game")
 screen.tracer(0)  # Disable automatic animation for faster drawing
-screen.listen()
 
-
-# Notify the user to start the game
-notification = Turtle()
-def start_notification():
-    notification.hideturtle()
-    notification.penup()
-    notification.color("white")
-    notification.goto(0, 0)
-    notification.write("Press SPACE to start the game", align="center", font=("Courier", 24, "normal"))
-
-start_notification() # Display the notification
-
-# Create the border for the game
+# Create game elements
 border = Border()
-
-#creating the bricks
 brick = Brick()
-
-#create a scorecard
 scoreboard = Scoreboard()
-
-
-#creating a pedal
 pedal = Pedal((0, -320))
-
-
-#creating a ball
 ball = Ball((0, 0))
 
-#Start the game function
+# Notification to start game
+notification = Turtle()
+notification.hideturtle()
+notification.penup()
+notification.color("white")
+
+def display_start_notification():
+    """Displays the start notification for the player."""
+    notification.clear()
+    notification.goto(0, -40)
+    notification.write("Press SPACE to start the game", align="center", font=("Courier", 24, "normal"))
+
+display_start_notification()  # Initial notification
+
+# Start game function
 def start_game():
     global is_game_on
     is_game_on = True
-    notification.clear()  # Clear the notification
-
+    scoreboard.new_turtle.clear()
+    notification.clear()  # Clear notification on game start
 
 # Bind start_game function to spacebar key
+screen.listen()
 screen.onkey(start_game, "space")
 
+# Paddle movement controls
+screen.onkeypress(pedal.move_left, "Left")
+screen.onkeypress(pedal.move_right, "Right")
 
+# Main game loop
 while True:
-
-    is_game_on = False # Stop the game
-
+    # Wait until game starts
     while not is_game_on:
         screen.update()
 
-    #binding the pedal to the left and right key
-    screen.onkeypress(pedal.move_left, "Left")
-    screen.onkeypress(pedal.move_right, "Right")
-
-    # Start the game
+    # Start game play loop
     while is_game_on:
         screen.update()
-        
-        # Move the ball 
-        ball.move() 
+        ball.move()
+        time.sleep(0.02)  # Lower sleep time for smoother animation
 
-
-        time.sleep(.02)
-
-         
-
-        # Detect collision with the wall
+        # Detect collision with the walls
         if ball.xcor() > 380 or ball.xcor() < -380:
             ball.bounce_x()
 
@@ -91,38 +72,63 @@ while True:
         if ball.ycor() > 290:
             ball.bounce_y()
 
-        #function to stop pedal from moving outof the screen
+        # Restrict paddle within screen limits
         if pedal.xcor() > 350:
-            pedal.goto(350, -320)
+            pedal.setx(350)
         if pedal.xcor() < -350:
-            pedal.goto(-350, -320)
+            pedal.setx(-350)
 
-        # Detect collision with the pedal
-        if ball.distance(pedal) < 50 and ball.ycor() < -300:
+        # Detect collision with the paddle
+        if ball.ycor() < -300 and pedal.xcor() - 70 < ball.xcor() < pedal.xcor() + 70:
             ball.bounce_y()
 
+        # Check if ball is out of bounds
         #checking the ball out of range
-        if ball.ycor() < -380 and ball.xcor() < -350:
-            if scoreboard.life > 0:
+        if ball.ycor() < -330 or ball.xcor() < -395:
+            if scoreboard.life > 1:
                 ball.reset_position()
                 scoreboard.life =scoreboard.life-1
-                scoreboard.update_scoreboard()
-
-            else:
-                scoreboard.game_over()
                 is_game_on = False
 
-        # Detect collision with the bricks
+            else:
+                scoreboard.life = 0
+
+                #reseting the ball position
+                ball.reset_position()
+
+                #recreating the bricks
+                brick.reset_bricks()
+
+                #updating the scoreboard
+                scoreboard.game_over()
+                #restarting the game
+                scoreboard.restart_game()
+                is_game_on = False
+
+            # update the scoreboard
+            scoreboard.update_scoreboard()
+
+            # Notify the user to start the game again
+            display_start_notification()
+            #reset the pedal position
+            pedal.reset_position()
+
+        # Detect collision with bricks
         for demo_brick in brick.bricks_list:
-            if ball.distance(demo_brick) < 20:
+            if ball.distance(demo_brick) < 30:
                 ball.bounce_y()
                 brick.remove_brick(demo_brick)
                 scoreboard.increase_score()
                 break
 
-    # Start the main loop
-    # screen.mainloop()
+        # Check if all bricks are cleared
+        if len(brick.bricks_list) == 0:
+            scoreboard.game_win()
+            brick.reset_bricks()
+            ball.reset_position()
+            pedal.reset_position()
+            is_game_on = False
+            display_start_notification()
+            break
 
-
-
-
+screen.mainloop()
