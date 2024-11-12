af3567f8f9e94b7e9bd48dfb5cf145a7405303b0
KushalRegmi61 <kushalbro82@gmail.com>
2024-11-12 21:07:08 +0545

    .

--- DAY_83_PortfolioWebsite/tempCodeRunnerFile.py
+++ DAY_83_PortfolioWebsite/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-projects
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, ValidationError, Email, Length
