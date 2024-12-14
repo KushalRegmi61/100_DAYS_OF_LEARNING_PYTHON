dba5df316277d6d70c92c0907c8a108933fa54b7
KushalRegmi61 <kushalbro82@gmail.com>
2024-12-14 20:36:37 +0545

    add the add_new product and update_product form

--- DAY_97_AnOnlineShop/forms.py
+++ DAY_97_AnOnlineShop/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, ValidationError, Email, Length
+from wtforms import StringField, SubmitField, PasswordField, FileField, DecimalField
+from wtforms.validators import DataRequired, URL, ValidationError, Email, Length, NumberRange
+from flask_wtf.file import FileField, FileAllowed
 from flask_ckeditor import CKEditorField
 import re
+
+
 
 
 # Custom validator function
@@ -40,3 +43,57 @@ class LoginForm(FlaskForm):
     email = StringField('Email', render_kw={"size": 30}, validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField(label="Log In", render_kw={"size": 30})
+
+
+class AddProductForm(FlaskForm):
+    name = StringField('Product Name',
+                       validators=[DataRequired()],
+                       render_kw={"size": 30, "placeholder": "Enter product name"}
+                       )
+    category = StringField('Category',
+                           validators=[DataRequired()],
+                           render_kw={"size": 30, "placeholder": "Enter product category Eg. Basin, Towel"}
+                           )
+    price = DecimalField('Price',
+                         validators=[DataRequired(), NumberRange(min=0, message="Price must be a positive number")],
+                         render_kw={"size": 30, "placeholder": "Enter product price in Nrs. Eg. 1000"}
+                         )
+    description = CKEditorField('Description',
+                                validators=[DataRequired()],
+                                render_kw={"size": 30, "placeholder": "Enter product description"}
+                                )
+    image_url = FileField('Image',
+                          validators=[DataRequired(), FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')],
+                          render_kw={"size": 30, "placeholder": "Choose an image file"}
+                          )
+    submit = SubmitField(label="Add Product", render_kw={"size": 30})
+
+# creating a form to update product
+class UpdateProductForm(FlaskForm):
+    name =  StringField('Product Name', 
+                        validators=[DataRequired()],
+                         render_kw={"size": 30, "placeholder": "Enter product name"}
+                         )
+    category = StringField('Category',
+                            validators=[DataRequired()],
+                            render_kw={"size": 30, "placeholder": "Enter product category Eg. Basin, Towel"}
+                            )
+    
+    price = StringField('Price',
+                        validators=[DataRequired()],
+                        render_kw={"size": 30, "placeholder": "Enter product price in Nrs. Eg. 1000"}
+                        )
+    
+    description = CKEditorField('Description', 
+                                validators=[DataRequired()],
+                                render_kw={"size": 30, "placeholder": "Enter product description"}
+                                )
+    
+    image_url = FileField('Image URL',
+                            validators=[DataRequired(), URL()],
+                            render_kw={"size": 30, "placeholder": "Choose an image file"}
+                            )
+    
+    submit = SubmitField(label="Update Product", render_kw={"size": 30})
+                          
+
